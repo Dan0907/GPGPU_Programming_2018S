09c33acfc2faf31be71e80d2ee1044c045a5430c
Dan <danny321974345@gmail.com>
2018-05-08 11:48:21 +0800

    update

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -8,11 +8,6 @@
 #include <thrust/execution_policy.h>
 
 #define BLOCK_SIZE 256
-#define SWAP(a, b) do {\
-	int *tmp_ptr = a;\
-	a = b;\
-	b = tmp_ptr;\
-} while (0)
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
@@ -34,16 +29,13 @@ void CountPosition1(const char *text, int *pos, int text_size)
 		result_ptr + text_size, result_ptr, result_ptr);
 }
 
-__global__ void scan(int *pos, int *tmp_arr, int n)
+__global__ void scan(int *pos, int n)
 {
 	const int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i >= n)
 		return;
-	if (i >= tmp_arr[i]) {
-		pos[i] = tmp_arr[i] + tmp_arr[i - tmp_arr[i]];
-	} else {
-		pos[i] = tmp_arr[i];
-	}
+	if (i >= pos[i])
+		pos[i] += pos[i - pos[i]];
 }
 
 __global__ void init(const char *text, int *pos, int n)
@@ -54,24 +46,22 @@ __global__ void init(const char *text, int *pos, int n)
 	if (text[i] != '\n')
 		pos[i] = 1;
 }
+__global__ void slow(int *pos, int n)
+{
+	const int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i >= n)
+		return;
+	int j;
+	for (j = 0; i - j >= 0 &&  pos[i - j] != 0; j++);
+	pos[i] = j;
+}
 
 void CountPosition2(const char *text, int *pos, int text_size)
 {
 	int i;
 	int grid_size = CeilDiv(text_size, BLOCK_SIZE);
-	int *ori_ptr = pos;
-	int *tmp_arr;
-	cudaMalloc(&tmp_arr, sizeof *tmp_arr * text_size);
 	init<<<grid_size, BLOCK_SIZE>>>(text, pos, text_size);
-	for (i = 0; i < 9; i++) {
-		SWAP(pos, tmp_arr);
-		scan<<<grid_size, BLOCK_SIZE>>>(pos, tmp_arr, text_size);
-	}
-	if (ori_ptr != pos) {
-		cudaMemcpy(ori_ptr, pos, sizeof *ori_ptr * text_size,
-			cudaMemcpyDeviceToDevice);
-		cudaFree(pos);
-	} else {
-		cudaFree(tmp_arr);
-	}
+	//slow<<<grid_size, BLOCK_SIZE>>>(pos, text_size);
+	for (i = 0; i < 9; i++)
+		scan<<<grid_size, BLOCK_SIZE>>>(pos, text_size);
 }
